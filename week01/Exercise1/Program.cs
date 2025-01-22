1b08c787a7b10db3e5148b877f56df63caea2fc2
Antonio Barros <antoniobarros1313@gmail.com>
2025-01-22 15:34:16 -0300

    Commiting exercise 1 & 2 of week 1 from CSE210 class

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name ?");
+        string firstName = Console.ReadLine();
+        Console.WriteLine("What is your last name ?");
+        string LastName = Console.ReadLine();
+        Console.Write($"Your name is {LastName}, {firstName} {LastName}.");
     }
 }
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,58 @@
 using System;
+using System.Linq.Expressions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter your grade porcentage: ");
+        string value = Console.ReadLine();
+        int grade = int.Parse(value);
+
+        int signal = grade;
+        string simbol = "";
+        if (signal >= 87 && signal < 90 || signal >= 77 && signal < 80 || signal >= 67 && signal < 70)
+        {
+            simbol = "+";
+        }
+        else if (signal < 93 && signal > 90||signal < 83 && signal > 80|| signal < 73 && signal > 70|| signal < 63 && signal > 60)
+        {
+            simbol = "-";
+        }
+        else
+        {
+            simbol = "";
+        }
+// This is the A grade code
+        if (grade >= 90)
+        {
+            Console.WriteLine($"Your grade is A{simbol}");
+        }
+        else if (grade >=80)
+        {
+            Console.WriteLine($"Your grade is B{simbol}");
+        }
+        else if (grade >=70)
+        {
+            Console.WriteLine($"Your grade is C{simbol}");
+        }
+        else if (grade >=60)
+        {
+            Console.WriteLine($"Your grade is D{simbol}");
+        }
+        else
+        {
+            Console.WriteLine("Your grade is F");
+        }
+
+        if (grade >= 70)
+        {
+            Console.Write("CONGRATULATIONS !!! YOU PASS THIS CLASS. ");
+        }
+        else
+        {
+            Console.Write("Unfortunately You do not pass the class... I encourage you trying! ");
+        }
+        
     }
 }
